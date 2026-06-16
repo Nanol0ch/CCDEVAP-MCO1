b0a5ac42112118162ed1c0521f5fd68c169d8536
pathakroshni19 <roshni_pathak@dlsu.edu.ph>
2026-06-16 23:57:37 +0800

    Create flightData.js

---
+++ Ross parts/flightData.js
@@ -0,0 +1,155 @@
+// Flight Dummy Data
+
+var flights = [
+
+    {
+        id: 1,
+        airline: "Philippine Airlines",
+        flightNumber: "PR 101",
+        origin: "MNL",
+        destination: "SIN",
+        departure: "06:00",
+        arrival: "09:45",
+        duration: "3h 45m",
+        durationMins: 225,
+        stops: 0,
+        price: 4500,
+        seats: 12
+    },
+
+    {
+        id: 2,
+        airline: "Cebu Pacific",
+        flightNumber: "5J 210",
+        origin: "MNL",
+        destination: "HKG",
+        departure: "08:30",
+        arrival: "11:10",
+        duration: "2h 40m",
+        durationMins: 160,
+        stops: 0,
+        price: 3200,
+        seats: 5
+    },
+
+    {
+        id: 3,
+        airline: "AirAsia",
+        flightNumber: "Z2 305",
+        origin: "CEB",
+        destination: "SIN",
+        departure: "13:15",
+        arrival: "17:00",
+        duration: "3h 45m",
+        durationMins: 225,
+        stops: 1,
+        price: 6800,
+        seats: 20
+    },
+
+    {
+        id: 4,
+        airline: "Qatar Airways",
+        flightNumber: "QR 928",
+        origin: "MNL",
+        destination: "DXB",
+        departure: "22:00",
+        arrival: "04:30",
+        duration: "9h 30m",
+        durationMins: 570,
+        stops: 1,
+        price: 18500,
+        seats: 8
+    },
+
+    {
+        id: 5,
+        airline: "Emirates",
+        flightNumber: "EK 333",
+        origin: "MNL",
+        destination: "DXB",
+        departure: "01:00",
+        arrival: "06:15",
+        duration: "9h 15m",
+        durationMins: 555,
+        stops: 0,
+        price: 22000,
+        seats: 3
+    },
+
+    {
+        id: 6,
+        airline: "Philippine Airlines",
+        flightNumber: "PR 504",
+        origin: "MNL",
+        destination: "NRT",
+        departure: "10:00",
+        arrival: "15:30",
+        duration: "4h 30m",
+        durationMins: 270,
+        stops: 0,
+        price: 12000,
+        seats: 15
+    },
+
+    {
+        id: 7,
+        airline: "Cebu Pacific",
+        flightNumber: "5J 820",
+        origin: "DVO",
+        destination: "MNL",
+        departure: "07:45",
+        arrival: "09:15",
+        duration: "1h 30m",
+        durationMins: 90,
+        stops: 0,
+        price: 2100,
+        seats: 30
+    },
+
+    {
+        id: 8,
+        airline: "AirAsia",
+        flightNumber: "Z2 614",
+        origin: "MNL",
+        destination: "SYD",
+        departure: "15:00",
+        arrival: "06:30",
+        duration: "8h 30m",
+        durationMins: 510,
+        stops: 1,
+        price: 15000,
+        seats: 10
+    },
+
+    {
+        id: 9,
+        airline: "Emirates",
+        flightNumber: "EK 219",
+        origin: "MNL",
+        destination: "DXB",
+        departure: "19:30",
+        arrival: "01:00",
+        duration: "9h 30m",
+        durationMins: 570,
+        stops: 0,
+        price: 35000,
+        seats: 2
+    },
+
+    {
+        id: 10,
+        airline: "Qatar Airways",
+        flightNumber: "QR 412",
+        origin: "CEB",
+        destination: "HKG",
+        departure: "11:00",
+        arrival: "13:30",
+        duration: "2h 30m",
+        durationMins: 150,
+        stops: 0,
+        price: 7500,
+        seats: 18
+    }
+
+];
